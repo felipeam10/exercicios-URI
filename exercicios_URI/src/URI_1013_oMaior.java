384908c2dc733d744b912fba83c8c9b4471e57d5
felipeam10 <43375643+felipeam10@users.noreply.github.com>
2019-10-29 23:19:21 -0300

    Add files via upload

---
+++ exercicios_URI/src/URI_1013_oMaior.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class URI_1013_oMaior {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b, c;
+		int maiorAB, maiorABC;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		
+		maiorAB = (a + b + Math.abs(a-b))/2;
+		maiorABC = (maiorAB + c + Math.abs(maiorAB - c)) / 2;
+		
+		System.out.printf("%d eh o maior\n", maiorABC);
+		
+		sc.close();
+		//teste
+	}
+}
